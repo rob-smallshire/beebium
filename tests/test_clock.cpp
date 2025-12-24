7827dc1a413c08adb5a14aac7f474521c97ffcc4
Robert Smallshire <robert@smallshire.org.uk>
2025-12-24 22:18:16 +0100

    Add GPL-3 license and copyright notices to all source files
    - Add COPYING.txt with full GPL-3 license text
    - Add copyright headers to all C/C++ source files in src/ and tests/
    - Add copyright headers to Swift client files in clients/
    - 6502 library files include dual copyright attributing Tom Seddon
      (original B2 author) alongside the Beebium copyright
    - Add pre-commit hook to verify copyright notices in staged source files

--- tests/test_clock.cpp
+++ tests/test_clock.cpp
@@ -1,3 +1,15 @@
+// Copyright © 2025 Robert Smallshire <robert@smallshire.org.uk>
+//
+// This file is part of Beebium.
+//
+// Beebium is free software: you can redistribute it and/or modify it under the terms of the
+// GNU General Public License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version. Beebium is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with Beebium.
+// If not, see <https://www.gnu.org/licenses/>.
+
 // Test compile-time clock subscription system
 
 #include <catch2/catch_test_macros.hpp>
